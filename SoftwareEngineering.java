f1434089c8769ce801b4a76deeecac14c607a979
leilaossi <101479758+leilaossi@users.noreply.github.com>
2022-03-12 21:24:54 -0500

    adds two numbers

---
+++ SoftwareEngineering.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SoftwareEngineering {
+
+    public static void main(String[] args){
+        int number1;
+        int number2;
+
+        Scanner scnr = new Scanner(System.in);
+
+        number1 = scnr.nextInt();
+        number2 = scnr.nextInt();
+
+        int sum = number1 + number2;
+
+        System.out.println(sum);
+    }
+}
